b62437db76171af50c8b62e56cbfeec99b5fded3
youngx123 <yangxyoung@gmail.com>
2022-10-28 10:40:18 +0800

    实现对尺度进行缩放

---
+++ readme.md
@@ -0,0 +1,113 @@
+<!-- ## update -->
+---
+
++ ## YUV
+YUV，分为三个分量，`Y`表示明亮度（Luminance或Luma），也就是灰度值；而`U`和`V` 表示的则是色度（Chrominance或Chroma),
+作用是描述影像色彩及饱和度，用于指定像素的颜色。
+
+__YUV格式有两大类__：`planar` 和 `packed`:
+`planar`的`YUV`格式，先连续存储所有像素点的`Y`，紧接着存储所有像素点的`U`，随后是所有像素点的`V`。
+
+`packed`的`YUV`格式，每个像素点的`Y,U,V`是连续交错存储的。
+
+**YUV与RGB的转换公式**
+
+```python
+B = Y + 1.7790 * (U - 128)
+G = Y - 0.3455 * (U - 128) - 0.7169 * (V - 128)
+R = Y + 1.4075 * (V - 128)
+
+RGB To YUV
+
+Y = 0.299 * R + 0.587 * G + 0.114 * B
+U = (B - Y) / 1.772
+v = (R - Y) / 1.402  (U ~(-128 - 127))
+```
++ ## YUYV4:2:2格式数据
+
+下面的四个像素为:`[Y0 U0 V0] [Y1 U1 V1] [Y2 U2 V2][Y3 U3 V3]`
+
+存放的码流为:`Y0 U0 Y1 V1 Y2 U2 Y3 V3`
+
+映射出像素点为:`[Y0 U0 V1][Y1 U0 v1] [Y2 U2 V3][Y3 U2 V3]`
+
+
++ ## yuv422p 内存分布
+```python
+                    w
+            +--------------------+
+            |Y0Y1Y2Y3...         |
+            |...                 |   h
+            |...                 |
+            |                    |
+            +--------------------+
+            |U0U1      |
+            |...       |   h
+            |...       |
+            |          |
+            +----------+
+            |V0V1      |
+            |...       |  h
+            |...       |
+            |          |
+            +----------+
+                w/2
+``` 
+
++ ## yuv420p 内存分布
+```python
+                    w
+            +--------------------+
+            |Y0Y1Y2Y3...         |
+            |...                 |   h
+            |...                 |
+            |                    |
+            +--------------------+
+            |U0U1      |
+            |...       |   h/2
+            |...       |
+            |          |
+            +----------+
+            |V0V1      |
+            |...       |  h/2
+            |...       |
+            |          |
+            +----------+
+                w/2
+```
+
+```python
+uint8_t* dst_y = yuvI420;
+int dst_y_stride = width;
+
+uint8_t* dst_u = yuvI420 + width * height;
+int dst_u_stride = (width + 1) / 2;
+
+uint8_t* dst_v = (yuvI420 + width * height) + dst_u_stride * ((height + 1) / 2);
+int dst_v_stride = (width + 1) / 2;
+
+libyuv::ARGBToI420(
+                argb.data, 
+                width * 4, 
+                
+                dst_y,
+                dst_y_stride, 
+                
+                dst_u, 
+                dst_u_stride, 
+                
+                dst_v, 
+                dst_v_stride, 
+                
+                width,                                          
+                height
+            );
+```
+
++ ## demo 
+因项目需要，使用`libyuv` 对 `yuyv(yuy2)`格式图像进行处理，主要实现为：
+
+`yuyv(yuy2) ---> I420 ---> I420Scale ---> BGRA`
+
+缩放后图片：
+<img style="margin:10px auto;display:block" width=224 src="./img/yuyv2i420_scale.jpg"/>
--- yuyv2i420.cpp
+++ yuyv2i420.cpp
@@ -11,24 +11,23 @@
 opencv : yuy2   --> cv::Mat  -->  RGB -- > resize 
 
 libyuv   : yuy2   --> I420  -->  I420Scale -- > ARGB 
-(在scale 过程中，不能对任意缩放系数进行缩放， 如0.5时报错， 0.1 , 0.2 , 0.175等都可以，具体原因有待进一步确认)
 */
 
 void opencvConvert(std::string yuvfile, int width, int height,  float scale_value=1)
 {
 
-        int framesize = width * height *2;
-        FILE *yuv422File = fopen(yuvfile.c_str(), "rb");
+	int framesize = width * height *2;
+	FILE *yuv422File = fopen(yuvfile.c_str(), "rb");
 
-        unsigned char *yuv422 = new uint8_t[framesize];
-        fread(yuv422, framesize * sizeof( unsigned char), 1, yuv422File);
-        fclose(yuv422File);
+	unsigned char *yuv422 = new uint8_t[framesize];
+	fread(yuv422, framesize * sizeof( unsigned char), 1, yuv422File);
+	fclose(yuv422File);
 
 
-        int resizeWidth =  (int)width*scale_value;     // 360
-        int resizeHeight=  (int)height*scale_value;   //180
+	int resizeWidth =  (int)width*scale_value;     // 360
+	int resizeHeight=  (int)height*scale_value;   //180
 
-        auto cv_start= std::chrono::steady_clock::now();
+	auto cv_start= std::chrono::steady_clock::now();
 
 	cv::Mat yuvImg, rgbImg, rgbImg_scale;
 	yuvImg.create(height, width, CV_8UC2);
@@ -36,33 +35,33 @@ void opencvConvert(std::string yuvfile, int width, int height,  float scale_valu
 
 	cv::cvtColor(yuvImg, rgbImg, cv::COLOR_YUV2BGR_YUYV);
         
-        auto cvbgr_start= std::chrono::steady_clock::now();
-        
-        
-        if (resizeHeight ==height){
-                std::chrono::duration<double> rgbconvert_spent = cvbgr_start - cv_start;
-                std::cout << "yuyv   convert  to  BGR :  " << rgbconvert_spent.count() * 1000 << "    ms\n\n ";
-        }
-        else {
-                cv::resize(rgbImg, rgbImg_scale, cv::Size(resizeWidth, resizeHeight));
-                auto cv_end= std::chrono::steady_clock::now();
+	auto cvbgr_start= std::chrono::steady_clock::now();
+	
+	
+	if (resizeHeight ==height){
+			std::chrono::duration<double> rgbconvert_spent = cvbgr_start - cv_start;
+			std::cout << "yuyv   convert  to  BGR :  " << rgbconvert_spent.count() * 1000 << "    ms\n\n ";
+	}
+	else {
+		cv::resize(rgbImg, rgbImg_scale, cv::Size(resizeWidth, resizeHeight));
+		auto cv_end= std::chrono::steady_clock::now();
 
 
-                std::chrono::duration<double> rgbconvert_spent = cvbgr_start - cv_start;
-                std::chrono::duration<double> rgbSclae_spent = cv_end - cvbgr_start;
-                std::cout << "yuyv   convert  to  BGR :  " << rgbconvert_spent.count() * 1000 << "    ms\n ";
-                std::cout << "BGR   scale  time :  " << rgbSclae_spent.count() * 1000 << "    ms\n ";
-                std::cout << "yuyv   -> BGR  -> Scale :  " << (rgbSclae_spent.count() + rgbconvert_spent.count()) * 1000 << "    ms\n\n ";
-        }
+		std::chrono::duration<double> rgbconvert_spent = cvbgr_start - cv_start;
+		std::chrono::duration<double> rgbSclae_spent = cv_end - cvbgr_start;
+		std::cout << "yuyv   convert  to  BGR :  " << rgbconvert_spent.count() * 1000 << "    ms\n ";
+		std::cout << "BGR   scale  time :  " << rgbSclae_spent.count() * 1000 << "    ms\n ";
+		std::cout << "yuyv   -> BGR  -> Scale :  " << (rgbSclae_spent.count() + rgbconvert_spent.count()) * 1000 << "    ms\n\n ";
+	}
 
-        if (resizeHeight !=height)
-        {
-                cv::imwrite("./yuyv2_rgb_cv_scale.jpg", rgbImg_scale);
-                cv::imwrite("./yuyv2_rgb_cv.jpg", rgbImg);
-        }
-        else{
-                cv::imwrite("./yuyv2_rgb_cv.jpg", rgbImg);
-        }
+	if (resizeHeight !=height)
+	{
+		cv::imwrite("./yuyv2_rgb_cv_scale.jpg", rgbImg_scale);
+		cv::imwrite("./yuyv2_rgb_cv.jpg", rgbImg);
+}
+	else{
+		cv::imwrite("./yuyv2_rgb_cv.jpg", rgbImg);
+	}
 }
 
 
@@ -70,19 +69,19 @@ void opencvConvert(std::string yuvfile, int width, int height,  float scale_valu
 void yuvLibConvert(std::string yuvfile, int width, int height,  float scale_value=1.)
 {
 
-        int framesize = width * height ;
-        FILE *yuv422File = fopen(yuvfile.c_str(), "rb");
+	int framesize = width * height ;
+	FILE *yuv422File = fopen(yuvfile.c_str(), "rb");
 
-        unsigned char *yuv422 = new uint8_t[framesize* 2];
-        fread(yuv422, framesize * 2 * sizeof( unsigned char), 1, yuv422File);
-        fclose(yuv422File);
+	unsigned char *yuv422 = new uint8_t[framesize* 2];
+	fread(yuv422, framesize * 2 * sizeof( unsigned char), 1, yuv422File);
+	fclose(yuv422File);
 
-       auto yuy2_start= std::chrono::steady_clock::now();
+    auto yuy2_start= std::chrono::steady_clock::now();
 
-       	uchar* yuvI420 = new uchar[framesize *3 /2 ];
+	uchar* yuvI420 = new uchar[framesize *3 /2 ];
 	memset(yuvI420, 0, sizeof(uchar) * (framesize *3  / 2 ) );
 
-        uint8_t* dst_y = yuvI420;
+	uint8_t* dst_y = yuvI420;
 	int dst_y_stride = width;
 
 	uchar* dst_u = yuvI420 + width * height;
@@ -92,146 +91,146 @@ void yuvLibConvert(std::string yuvfile, int width, int height,  float scale_valu
 	int dst_v_stride = (width + 1) / 2;
 
 	libyuv::YUY2ToI420(
-                                                yuv422, 
-                                                width * 2, 
-                                                
-                                                dst_y, 
-                                                dst_y_stride, 
-                                                
-                                                dst_u, 
-                                                dst_u_stride, 
-                                                
-                                                dst_v, 
-                                                dst_v_stride, 
-                                                
-                                                width, 
-                                                height
-                                        );
-        auto yuy2_end= std::chrono::steady_clock::now();
-        std::chrono::duration<double> yuyv_spent = yuy2_end - yuy2_start;
-        // std::cout << "yuyv To yuv422  convert   time :  " << yuyv_spent.count() * 1000 << "    ms \n ";
+					yuv422, 
+					width * 2, 
+					
+					dst_y, 
+					dst_y_stride, 
+					
+					dst_u, 
+					dst_u_stride, 
+					
+					dst_v, 
+					dst_v_stride, 
+					
+					width, 
+					height
+			);
+	auto yuy2_end= std::chrono::steady_clock::now();
+	std::chrono::duration<double> yuyv_spent = yuy2_end - yuy2_start;
+	// std::cout << "yuyv To yuv422  convert   time :  " << yuyv_spent.count() * 1000 << "    ms \n ";
 
-        // I420 convert to BGR  and save BGR  image to disk
-        cv::Mat  matI420 = cv::Mat(height, width, CV_8UC4, cv::Scalar::all(0));
+	// I420 convert to BGR  and save BGR  image to disk
+	cv::Mat  matI420 = cv::Mat(height, width, CV_8UC4, cv::Scalar::all(0));
 
-        auto rgbCovnert_start = std::chrono::steady_clock::now();
+	auto rgbCovnert_start = std::chrono::steady_clock::now();
 
-        libyuv::I420ToARGB(
-                                                dst_y, 
-                                                dst_y_stride, 
-                                                
-                                                dst_u, 
-                                                dst_u_stride, 
+	libyuv::I420ToARGB(
+					dst_y, 
+					dst_y_stride, 
+					
+					dst_u, 
+					dst_u_stride, 
 
-                                                dst_v, 
-                                                dst_v_stride,
+					dst_v, 
+					dst_v_stride,
 
-                                                matI420.data, 
-                                                width * 4, 
-                                                
-                                                width, 
-                                                height
-                                        );
+					matI420.data, 
+					width * 4, 
+					
+					width, 
+					height
+			);
 
-        auto rgbCovnert_end= std::chrono::steady_clock::now();
-        std::chrono::duration<double> rgbCovnert_spent = rgbCovnert_end - rgbCovnert_start;
-        cv::imwrite("./yuyv2i420_origin_size.jpg", matI420);
-
-
-        int dstWidth =  (int)width*scale_value;    // 360
-        int dstHeight= (int)height*scale_value;   //180
-        if (dstWidth ==width){
-                std::cout << "yuy2 -> yuv420   time :  " <<  yuyv_spent.count() * 1000 << "    ms \n ";
-                std::cout << "yuv420 -> BGR  convert   time :  " <<  rgbCovnert_spent.count() * 1000 << "    ms \n ";
-                std::cout << "yuy2 -> yuv420 -> BGR  convert   time :  " << ( yuyv_spent.count() + rgbCovnert_spent.count() )* 1000 << "    ms \n \n";
-        }
-        else
-        {
-               std::cout<<"----- I420  scale to width  :  "<< dstWidth << "       height   :  " <<dstHeight<<"\n\n";
-                
-                auto yuv422_scale_start= std::chrono::steady_clock::now();
+	auto rgbCovnert_end= std::chrono::steady_clock::now();
+	std::chrono::duration<double> rgbCovnert_spent = rgbCovnert_end - rgbCovnert_start;
+	cv::imwrite("./yuyv2i420_origin_size.jpg", matI420);
 
 
-                uint8_t *yuv420_Scale = new uint8_t[dstWidth * dstHeight* 3 / 2];
-
-                uint8_t* scale_dst_y = yuv420_Scale;
-                int scale_dst_y_stride = dstWidth;
-
-                uint8_t* scale_dst_u = yuv420_Scale + dstWidth*dstHeight;
-                int scale_dst_u_stride = (dstWidth+1)/2;
-
-                uint8_t* scale_dst_v = (yuv420_Scale + dstWidth*dstHeight) + dst_u_stride * ((dstHeight + 1) / 2);
-                int scale_dst_v_stride =  (dstWidth+1)/2;
-
-                //std::cout<<"----- I420  scale  :  "<<std::endl;
-                libyuv::I420Scale(
-                                                dst_y, 
-                                                dst_y_stride,
-                                                
-                                                dst_u, 
-                                                dst_u_stride, 
-                                                
-                                                dst_v, 
-                                                dst_v_stride,
-                                                
-                                                width, 
-                                                height,
-                                                
-                                                scale_dst_y,
-                                                scale_dst_y_stride,
-
-                                                scale_dst_u,
-                                                scale_dst_u_stride,
-
-                                                scale_dst_v,
-                                                scale_dst_v_stride,
-
-                                                dstWidth,
-                                                dstHeight,
-
-                                                libyuv::kFilterBilinear
-                                        );
-                auto yuv422_scale_end= std::chrono::steady_clock::now();
-                std::chrono::duration<double> yuv422Scale_spent = yuv422_scale_end - yuv422_scale_start;
+	int dstWidth =  (int)width*scale_value;    // 360
+	int dstHeight= (int)height*scale_value;   //180
+	if (dstWidth ==width){
+			std::cout << "yuy2 -> yuv420   time :  " <<  yuyv_spent.count() * 1000 << "    ms \n ";
+			std::cout << "yuv420 -> BGR  convert   time :  " <<  rgbCovnert_spent.count() * 1000 << "    ms \n ";
+			std::cout << "yuy2 -> yuv420 -> BGR  convert   time :  " << ( yuyv_spent.count() + rgbCovnert_spent.count() )* 1000 << "    ms \n \n";
+	}
+	else
+	{
+	   std::cout<<"----- I420  scale to width  :  "<< dstWidth << "       height   :  " <<dstHeight<<"\n\n";
+		
+		auto yuv422_scale_start= std::chrono::steady_clock::now();
 
 
-                cv::Mat  matI420_scale = cv::Mat(dstHeight, dstWidth, CV_8UC4, cv::Scalar::all(0));
+		uint8_t *yuv420_Scale = new uint8_t[dstWidth * dstHeight* 3 / 2];
 
-                auto rgbCovnert_scale_start= std::chrono::steady_clock::now();
-	        libyuv::I420ToARGB(
-                                                        scale_dst_y, 
-                                                        scale_dst_y_stride, 
-                                                        
-                                                        scale_dst_u, 
-                                                        scale_dst_u_stride, 
+		uint8_t* scale_dst_y = yuv420_Scale;
+		int scale_dst_y_stride = dstWidth;
 
-                                                        scale_dst_v, 
-                                                        scale_dst_v_stride,
+		uint8_t* scale_dst_u = yuv420_Scale + dstWidth*dstHeight;
+		int scale_dst_u_stride = (dstWidth+1)/2;
 
-						        matI420_scale.data, 
-                                                        dstWidth * 4, 
-                                                        
-                                                        dstWidth, 
-                                                        dstHeight
-                                                );
-                auto rgbCovnert_scale_end= std::chrono::steady_clock::now();
-                std::chrono::duration<double> rgbCovnert_scale_spent = rgbCovnert_scale_end - rgbCovnert_scale_start;
+		uint8_t* scale_dst_v = (yuv420_Scale + dstWidth*dstHeight) + scale_dst_u_stride * ((dstHeight + 1) / 2);
+		int scale_dst_v_stride =  (dstWidth+1)/2;
 
-                cv::imwrite("./yuyv2i420_scale.jpg", matI420_scale);
+		//std::cout<<"----- I420  scale  :  "<<std::endl;
+		libyuv::I420Scale(
+						dst_y, 
+						dst_y_stride,
+						
+						dst_u, 
+						dst_u_stride, 
+						
+						dst_v, 
+						dst_v_stride,
+						
+						width, 
+						height,
+						
+						scale_dst_y,
+						scale_dst_y_stride,
+
+						scale_dst_u,
+						scale_dst_u_stride,
+
+						scale_dst_v,
+						scale_dst_v_stride,
+
+						dstWidth,
+						dstHeight,
+
+						libyuv::kFilterBilinear
+				);
+		auto yuv422_scale_end= std::chrono::steady_clock::now();
+		std::chrono::duration<double> yuv422Scale_spent = yuv422_scale_end - yuv422_scale_start;
 
 
-                std::cout << "yuy2 -> yuv420   time :  " <<  yuyv_spent.count() * 1000 << "    ms \n ";
-                std::cout << "yuv420 -> BGR  convert   time :  " <<  rgbCovnert_spent.count() * 1000 << "    ms \n \n";
-                std::cout << "yuv422  Scale    time :  " << yuv422Scale_spent.count() * 1000 << "    ms  \n ";
-                std::cout << "yuv422  Scale  -> BGR  time :  " << rgbCovnert_scale_spent.count() * 1000 << "    ms  \n\n ";
+		cv::Mat  matI420_scale = cv::Mat(dstHeight, dstWidth, CV_8UC4, cv::Scalar::all(0));
 
-                std::cout << "yuy2 -> yuv420 -> yuv422  Scale   time :  " << ( yuyv_spent.count() + yuv422Scale_spent.count())* 1000 << "    ms \n ";
+		auto rgbCovnert_scale_start= std::chrono::steady_clock::now();
+		libyuv::I420ToARGB(
+							scale_dst_y, 
+							scale_dst_y_stride, 
+							
+							scale_dst_u, 
+							scale_dst_u_stride, 
 
-                std::cout << "yuy2 -> yuv420 -> yuv422  Scale  -> BGR  convert    time :  " <<
-                                                                 ( yuyv_spent.count() +  yuv422Scale_spent.count() + rgbCovnert_scale_spent.count())* 1000 << "    ms \n\n ";
+							scale_dst_v, 
+							scale_dst_v_stride,
+
+							matI420_scale.data, 
+							dstWidth * 4, 
+							
+							dstWidth, 
+							dstHeight
+						);
+						
+		auto rgbCovnert_scale_end= std::chrono::steady_clock::now();
+		std::chrono::duration<double> rgbCovnert_scale_spent = rgbCovnert_scale_end - rgbCovnert_scale_start;
+
+		cv::imwrite("./yuyv2i420_scale.jpg", matI420_scale);
 
 
-        }
+		std::cout << "yuy2 -> yuv420   time :  " <<  yuyv_spent.count() * 1000 << "    ms \n ";
+		std::cout << "yuv420 -> BGR  convert   time :  " <<  rgbCovnert_spent.count() * 1000 << "    ms \n \n";
+		std::cout << "yuv422  Scale    time :  " << yuv422Scale_spent.count() * 1000 << "    ms  \n ";
+		std::cout << "yuv422  Scale  -> BGR  time :  " << rgbCovnert_scale_spent.count() * 1000 << "    ms  \n\n ";
+
+		std::cout << "yuy2 -> yuv420 -> yuv422  Scale   time :  " << ( yuyv_spent.count() + yuv422Scale_spent.count())* 1000 << "    ms \n ";
+
+		std::cout << "yuy2 -> yuv420 -> yuv422  Scale  -> BGR  convert    time :  " <<
+															 ( yuyv_spent.count() +  yuv422Scale_spent.count() + rgbCovnert_scale_spent.count())* 1000 << "    ms \n\n ";
+
+	}
 
 }
 
@@ -240,10 +239,10 @@ int main(int argc, char *argv[]) {
         float scale_value ;
         if (argc ==1)
         {
-                scale_value = 1;
+			scale_value = 1;
         }
         else{
-                scale_value = std::stof(argv[1]);
+			scale_value = std::stof(argv[1]);
         }
 
         std::string yuvfile = "./00316_3.yuyv";
